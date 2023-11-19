f417f27f8f173a146be8133e768397765c5c712e
bluelisandro <nano0117@gmail.com>
2023-11-19 03:44:57 -0500

    Add table componenet, has syntax errors

--- components/table.tsx
+++ components/table.tsx
@@ -5,61 +5,56 @@ import RefreshButton from './refresh-button'
 import { seed } from '@/lib/seed'
 
 export default async function Table() {
-  let data
+  let data: any
   let startTime = Date.now()
 
-  try {
-    data = await sql`SELECT * FROM users`
-  } catch (e: any) {
-    if (e.message === `relation "users" does not exist`) {
-      console.log(
-        'Table does not exist, creating and seeding it with dummy data now...'
-      )
-      // Table is not created yet
-      await seed()
-      startTime = Date.now()
-      data = await sql`SELECT * FROM users`
-    } else {
-      throw e
-    }
-  }
+  // try {
+  data = await sql`SELECT pid, fname, lname FROM patient`
+  // } catch (e: any) {
+  //   if (e) {
+  //     throw e
+  //   }
+  // }
 
-  const { rows: users } = data
-  const duration = Date.now() - startTime
+  const { rows: patients } = data
+  // const duration = Date.now() - startTime
 
   return (
     <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
       <div className="flex justify-between items-center mb-4">
         <div className="space-y-1">
-          <h2 className="text-xl font-semibold">Recent Users</h2>
-          <p className="text-sm text-gray-500">
-            Fetched {users.length} users in {duration}ms
-          </p>
+          <h2 className="text-xl font-semibold">Patients</h2>
         </div>
         <RefreshButton />
       </div>
       <div className="divide-y divide-gray-900/5">
-        {users.map((user) => (
-          <div
-            key={user.name}
-            className="flex items-center justify-between py-3"
-          >
-            <div className="flex items-center space-x-4">
-              <Image
-                src={user.image}
-                alt={user.name}
-                width={48}
-                height={48}
-                className="rounded-full ring-1 ring-gray-900/5"
-              />
-              <div className="space-y-1">
-                <p className="font-medium leading-none">{user.name}</p>
-                <p className="text-sm text-gray-500">{user.email}</p>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {patients.map((patient) => (
+              <div
+                key={patient.pid}
+                className="flex items-center justify-between py-3"
+              >
+                <div className="flex items-center space-x-4">
+                  <div className="space-y-1">
+                    <tr>
+                    <td className="font-medium leading-none">{patient.pid}</td>
+                    <td className="text-sm text-gray-500">{patient.fname}</td>
+                    <td className="text-sm text-gray-500">{patient.lname}</td>
+                    </tr>
+                  </div>
+                </div>
               </div>
-            </div>
-            <p className="text-sm text-gray-500">{timeAgo(user.createdAt)}</p>
-          </div>
-        ))}
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   )
